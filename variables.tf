edcddde4400eb11df5ec46299787ea9bfeb74a01
regulapati-n <regulapati.n@northeastern.edu>
2024-02-13 23:06:06 -0500

    Added Terraform config

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project_id" {
+  description = "GCP project ID"
+}
+
+variable "region" {
+  description = "GCP region"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+}
+
+variable "webapp_subnet_name" {
+  description = "Name of the webapp subnet"
+}
+
+variable "webapp_subnet_cidr" {
+  description = "CIDR range for the webapp subnet"
+}
+
+variable "db_subnet_name" {
+  description = "Name of the db subnet"
+}
+
+variable "db_subnet_cidr" {
+  description = "CIDR range for the db subnet"
+}
